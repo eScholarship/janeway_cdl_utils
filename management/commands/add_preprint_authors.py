05a2107f2bcceffd6df5570843fee37a049cfdeb
Esther Verreau <esther.verreau@ucop.edu>
2023-08-24 10:15:33 -0500

    implement add_preprint_authors command that adds authors defined in a csv

---
+++ management/commands/add_preprint_authors.py
@@ -0,0 +1,56 @@
+from django.core.management.base import BaseCommand, CommandError
+
+import csv
+
+from repository.models import Preprint, PreprintAuthor
+from core.models import Account
+
+class Command(BaseCommand):
+    """Adds a list of authors to preprints given a csv"""
+    help = "Adds a list of authors to preprints given a csv"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "import_file", help="path to an import file containing the authors to add", type=str
+        )
+        parser.add_argument(
+            '--overwrite',
+            action='store_true',
+            help="Remove existing authors from a preprint before importing",
+        )
+
+
+    def handle(self, *args, **options):
+        import_file = options.get("import_file")
+        overwrite = options["overwrite"]
+        rows = []
+        preprints = []
+
+        with open(import_file, 'r') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            for r in reader:
+                pk = r["Preprint ID"]
+                if not Preprint.objects.filter(pk=pk).exists():
+                    raise CommandError(f'Preprint {pk} does not exist')
+                if not overwrite and PreprintAuthor.objects.filter(preprint__pk=pk, account__email=r["Email"]).exists():
+                    raise CommandError(f'Account with email {r["Email"]} is already an author on Preprint {pk}. Did you mean to use *overwrite*?')
+                rows.append(r)
+                if overwrite and not pk in preprints:
+                    preprints.append(pk)
+ 
+        if overwrite:
+            for p in preprints:
+                preprint = Preprint.objects.get(pk=p)
+                for a in preprint.preprintauthor_set.all():
+                    a.delete()
+        
+        for r in rows:
+            preprint = Preprint.objects.get(pk=r["Preprint ID"])
+            defaults = {'username': r["Email"], 'first_name': r["First Name"], 'middle_name': r["Middle Name"], 'last_name': r["Last Name"], 'institution': r["Affiliation"]}
+            account, created = Account.objects.get_or_create(email=r["Email"], defaults=defaults)
+            author, created = PreprintAuthor.objects.get_or_create(preprint=preprint,
+                                                                   account=account,
+                                                                   defaults= {'order': r["Author Order"],
+                                                                              'affiliation': r["Affiliation"]})
+            if created:
+                author.save()
--- tests.py
+++ tests.py
@@ -1,12 +1,26 @@
 from django.test import TestCase
+from django.core.management.base import CommandError
 
 from utils.testing import helpers
 
 from submission.models import Article
 from journal.models import Journal
+from core.models import Account
+from repository.models import PreprintAuthor
 
 from io import StringIO
 from django.core.management import call_command
+
+import os
+
+test_users = """Preprint ID,Author Order,First Name,Middle Name,Last Name,Email,Affiliation,CDL Notes
+{id},1,Brian,Test,Tester,tinguru@mac.com,Test University,"existing user, same acct data"
+{id},2,Test,Q,Test,tester2@mailinator.com,Test University,"existing user, different acct data"
+{id},3,Bob,,Tester,bobresearcher@mailinator.com,Test University,existing user (preprint owner)
+{id},4,Wei,,Zhang,eaauthor-test2@mailinator.com,Test University,new user
+{id},5,Fatima,,Kumari,eaauthor-test3@mailinator.com,Test University,new user
+{id},6,Bobby,B.,Researcher,bobresearcher+3@mailinator.com,Test University,"existing user, different acct data"
+"""
 
 class TestDeleteJournals(TestCase):
 
@@ -44,3 +58,126 @@ class TestDeleteJournals(TestCase):
         self.assertEqual(Journal.objects.count(), 2)
         self.assertEqual(Article.objects.filter(journal=None).count(), 0)
         self.assertEqual(Article.objects.count(), 1)
+
+class TestAddPreprintAuthors(TestCase):
+
+    def create_account(self, email, fname, mname, lname, institution):
+        a = Account.objects.create_user(email=email)
+        a.first_name = fname
+        a.middle_name = mname
+        a.last_name = lname
+        a.institution = institution
+        a.save()
+        return a
+
+    def setUp(self):
+        self.press = helpers.create_press()
+        self.repo, subject = helpers.create_repository(self.press, [], [])
+        self.user0 = self.create_account("bobresearcher@mailinator.com", "Bob", "", "Tester", "Test University")
+        self.user1 = self.create_account("tinguru@mac.com", "Brian", "Test", "Tester", "Test University")
+        self.user2 = self.create_account("tester2@mailinator.com", "Other", "M", "Name", "Other University")
+        self.user3 = self.create_account("bobresearcher+3@mailinator.com", "Robert", "B", "Researcher", "Test University")
+        self.user4 = self.create_account("other.user@test.org", "Other", "", "User", "Other University")
+
+        self.preprint = helpers.create_preprint(self.repo, self.user4, subject)
+        self.preprint.owner = self.user0
+        self.preprint.save()
+
+    def call_command(self, *args, **kwargs):
+        out = StringIO()
+        call_command(
+            "add_preprint_authors",
+            *args,
+            stdout=out,
+            stderr=StringIO(),
+            **kwargs,
+        )
+        return out.getvalue()
+
+
+    def test_append_authors(self):
+        with open("test.csv", 'w') as t:
+            t.write(test_users.format(id=self.preprint.pk))
+        out = self.call_command("test.csv")
+
+        self.assertEqual(len(self.preprint.authors), 7)
+
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user4).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user3).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user2).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user1).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user0).count(), 1)
+
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account__email="eaauthor-test2@mailinator.com").count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account__email="eaauthor-test3@mailinator.com").count(), 1)
+
+        u1 = Account.objects.get(email="tester2@mailinator.com")
+        self.assertEqual(u1.first_name, "Other")
+        self.assertEqual(u1.middle_name, "M")
+        self.assertEqual(u1.last_name, "Name")
+        self.assertEqual(u1.institution, "Other University")
+
+        p1 = PreprintAuthor.objects.get(preprint=self.preprint, account=u1)
+        self.assertEqual(p1.order, 2)
+        self.assertEqual(p1.affiliation, "Test University")
+
+        u2 = Account.objects.get(email="eaauthor-test3@mailinator.com")
+        self.assertEqual(u2.first_name, "Fatima")
+        self.assertEqual(u2.middle_name, "")
+        self.assertEqual(u2.last_name, "Kumari")
+        self.assertEqual(u2.institution, "Test University")
+
+        p2 = PreprintAuthor.objects.get(preprint=self.preprint, account=u2)
+        self.assertEqual(p2.order, 5)
+        self.assertEqual(p2.affiliation, "Test University")
+
+    def test_overwrite_authors(self):
+        with open("test.csv", 'w') as t:
+            t.write(test_users.format(id=self.preprint.pk))
+        out = self.call_command("test.csv", "--overwrite")
+        self.assertEqual(len(self.preprint.authors), 6)
+
+        self.assertFalse(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user4).exists())
+
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user3).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user2).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user1).count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user0).count(), 1)
+
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account__email="eaauthor-test2@mailinator.com").count(), 1)
+        self.assertEqual(PreprintAuthor.objects.filter(preprint=self.preprint, account__email="eaauthor-test3@mailinator.com").count(), 1)
+
+    def test_no_preprint(self):
+        with open("test.csv", 'w') as t:
+            t.write(test_users.format(id=1000))
+        with self.assertRaises(CommandError):
+            out = self.call_command("test.csv")
+
+        self.assertEqual(len(self.preprint.authors), 1)
+
+    def test_author_exists(self):
+        with open("test.csv", 'w') as t:
+            t.write(test_users.format(id=self.preprint.pk))
+        author = PreprintAuthor.objects.create(preprint=self.preprint, account=self.user0, order=1)
+        with self.assertRaises(CommandError):
+            out = self.call_command("test.csv")
+
+        self.assertEqual(len(self.preprint.authors), 2)
+
+    def test_author_exists_overwrite(self):
+        with open("test.csv", 'w') as t:
+            t.write(test_users.format(id=self.preprint.pk))
+        author = PreprintAuthor.objects.create(preprint=self.preprint, account=self.user0, order=1)
+        out = self.call_command("test.csv", "--overwrite")
+
+        self.assertEqual(len(self.preprint.authors), 6)
+
+        self.assertFalse(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user4).exists())
+
+        self.assertTrue(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user3).exists())
+        self.assertTrue(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user2).exists())
+        self.assertTrue(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user1).exists())
+        self.assertTrue(PreprintAuthor.objects.filter(preprint=self.preprint, account=self.user0).exists())
+
+        self.assertTrue(PreprintAuthor.objects.filter(preprint=self.preprint, account__email="eaauthor-test2@mailinator.com").exists())
+        self.assertTrue(PreprintAuthor.objects.filter(preprint=self.preprint, account__email="eaauthor-test3@mailinator.com").exists())
