e943ea3d06cad8d246a09236894c948012613f66
Esther Verreau <esther.verreau@ucop.edu>
2023-10-10 16:28:35 -0500

    don't worry about those old zip dirs here

--- management/commands/find_unlinked_files.py
+++ management/commands/find_unlinked_files.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from submission.models import Article
 from core.models import File, FileHistory
 
-import os, re
+import os
 
 from .boolean_input import *
 
@@ -54,17 +54,5 @@ class Command(BaseCommand):
         for d in os.listdir(af_dir):
             file_dir = os.path.join(af_dir, d)
             count += self.search_dir(file_dir, int(d), delete)
-
-        # search 'auxillary' directories (I don't know where these come from and they might all be cruft)
-        file_dir = os.path.join(settings.BASE_DIR, 'files')
-        for d in os.listdir(file_dir):
-            result = re.search(r'^(\d+) - .*', d)
-            if result:
-                i = int(result.groups(0)[0])
-                dname = os.path.join(file_dir, d, str(i))
-                if os.path.isdir(dname):
-                    count += self.search_dir(dname, i, delete)
-                else:
-                    print(f"Directory not found: {dname}")
 
         print(f"Found {count} files not referenced in the database")
