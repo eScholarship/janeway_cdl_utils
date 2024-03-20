417c728b40f3df31ba9974a7ac67cc6bdaf5a78d
Esther Verreau <esther.verreau@ucop.edu>
2024-03-20 09:42:12 -0500

    catch connection errors

--- management/commands/backfill_review_assignments.py
+++ management/commands/backfill_review_assignments.py
@@ -70,36 +70,39 @@ class Command(BaseCommand):
         for article in Article.objects.filter(journal=journal):
             ojs_id = self.get_ojs_id(article)
             round_url = f"{ojs_url}/{ojs_code}/articles/{ojs_id}/rounds"
-            rounds = self.get_ids(requests.get(round_url).text)
-            for r in rounds:
-                assignments_url = f"{round_url}/{r}/assignments"
-                assignments = self.get_ids(requests.get(assignments_url).text)
-                for a_id in assignments:
-                    assignment = json.loads(requests.get(f"{assignments_url}/{a_id}").text)
-                    # only try and find the matching review assignment if there are 
-                    # comments to place
-                    if len(assignment["comments"]) > 0:
-                        # some assignments have the exact same date requested but I haven't found any that also have the exact date completed
-                        ra = ReviewAssignment.objects.filter(date_requested=assignment["date_assigned"], date_complete=assignment["date_completed"])
-                        # if the above doesn't produce a unique assignment report an error
-                        if ra.count() > 1:
-                            print(f"ERROR: found multiple assignments {ra}")
-                            print(assignment)
-                        elif ra.count() < 1:
-                            print("ERROR: didn't find assignment")
-                            print(assignment)
-                        else:
-                            r = ra[0]
-                            for c in assignment["comments"]:
-                                # the first item that is not visible to the author goes into comments_for_editor
-                                # else they go in a form answer marked as author_can_see=False
-                                if not c["visible_to_author"] and (not r.comments_for_editor or len(r.comments_for_editor) == 0):
-                                    r.comments_for_editor = self.strip_html(c["comments"])
-                                else:
-                                    answer = ReviewAssignmentAnswer.objects.create(assignment=r,
-                                                                                   original_element=form_element,
-                                                                                   author_can_see=c["visible_to_author"],
-                                                                                   answer=self.strip_html(c["comments"]))
-                                    form_element.snapshot(answer)
+            try:
+                rounds = self.get_ids(requests.get(round_url).text)
+                for r in rounds:
+                    assignments_url = f"{round_url}/{r}/assignments"
+                    assignments = self.get_ids(requests.get(assignments_url).text)
+                    for a_id in assignments:
+                        assignment = json.loads(requests.get(f"{assignments_url}/{a_id}").text)
+                        # only try and find the matching review assignment if there are
+                        # comments to place
+                        if len(assignment["comments"]) > 0:
+                            # some assignments have the exact same date requested but I haven't found any that also have the exact date completed
+                            ra = ReviewAssignment.objects.filter(date_requested=assignment["date_assigned"], date_complete=assignment["date_completed"])
+                            # if the above doesn't produce a unique assignment report an error
+                            if ra.count() > 1:
+                                print(f"ERROR: found multiple assignments {ra}")
+                                print(assignment)
+                            elif ra.count() < 1:
+                                print("ERROR: didn't find assignment")
+                                print(assignment)
+                            else:
+                                r = ra[0]
+                                for c in assignment["comments"]:
+                                    # the first item that is not visible to the author goes into comments_for_editor
+                                    # else they go in a form answer marked as author_can_see=False
+                                    if not c["visible_to_author"] and (not r.comments_for_editor or len(r.comments_for_editor) == 0):
+                                        r.comments_for_editor = self.strip_html(c["comments"])
+                                    else:
+                                        answer = ReviewAssignmentAnswer.objects.create(assignment=r,
+                                                                                       original_element=form_element,
+                                                                                       author_can_see=c["visible_to_author"],
+                                                                                       answer=self.strip_html(c["comments"]))
+                                        form_element.snapshot(answer)
 
-                            r.save()
+                                r.save()
+            except requests.exceptions.RequestException as e:
+                raise CommandError(f'An error occurred: {e}')
