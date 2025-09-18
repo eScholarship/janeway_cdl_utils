98aec5fbed98970ef69c53c8fdbea5d459274391
Esther Verreau <esther.verreau@ucop.edu>
2025-09-18 14:42:32 -0500

    add disable login middleware

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def cdl_utils_manager(request):
     return HttpResponse("CDL Utils Plugin")
+
+class LoginDisabledView(TemplateView):
+    template_name = "cdl_utils/login_disabled.html"
